0133134342aff65e7bddf1cc73f13a4728166b5b
ernurator <isenbaev121102ernur@gmail.com>
2020-05-17 22:36:01 +0600

    AI upgraded

--- multi_auto.py
+++ multi_auto.py
@@ -5,37 +5,99 @@ from room_events import RoomEvents
 from tank_drawings import draw_tank, draw_bullet, drawScoreboard
 #pylint: disable=no-member, too-many-function-args
 
-RADIUS = 90
+RADIUS = 200
+
+def opposite_direction(direction):
+    if direction == 'LEFT':
+        return 'RIGHT'
+    if direction == 'RIGHT':
+        return 'LEFT'
+    if direction == 'UP':
+        return 'DOWN'
+    if direction == 'DOWN':
+        return 'UP'
+
+def create_rect(speed, x, y, width, height, direction, **kwargs):
+    rects = []
+    for i in range(1, 11):
+        if direction == 'LEFT':
+            rects.append(pygame.Rect(int(x - speed * i / 10), y, width, height))
+        if direction == 'RIGHT':
+            rects.append(pygame.Rect(int(x + speed * i / 10), y, width, height))
+        if direction == 'UP':
+            rects.append(pygame.Rect(x, int(y - speed * i / 10), width, height))
+        if direction == 'DOWN':
+            rects.append(pygame.Rect(x, int(y + speed * i / 10), width, height))
+    return rects
 
 class AI:
     def __init__(self, name):
         self.name = name
         self.fire = False
         self.turn_direction = 'UP'
-    
+
     def start(self, tanks, bullets):
         me = next((x for x in tanks if x['id'] == self.name), None)
         if not me: return
         bullets = [i for i in bullets if i['owner'] != self.name]
+        dangerous_bullets = []
+        me_rects = create_rect(50, **me)
         for bullet in bullets:
+            bullet_rects = create_rect(100, **bullet)
             dist_x = bullet['x'] - me['x']
             dist_y = bullet['y'] - me['y']
-            if bullet['direction'] == 'UP':
+            if bullet['direction'] == 'UP' and (me['direction'] == 'UP' or me['direction'] == 'DOWN'):
                 if 0 < dist_y < RADIUS + me['height'] and -bullet['width'] < dist_x < me['width']:
-                    if me['direction'] == 'UP' or me['direction'] == 'DOWN':
-                        self.turn_direction = 'RIGHT'
-            if bullet['direction'] == 'DOWN':
-                if -me['height'] - RADIUS < dist_y < 0 and -bullet['width'] < dist_x < me['width']:
-                    if me['direction'] == 'UP' or me['direction'] == 'DOWN':
-                        self.turn_direction = 'RIGHT'
-            if bullet['direction'] == 'LEFT':
+                    direction = 'RIGHT' if dist_x + bullet['width'] // 2 < me['width'] // 2 else 'LEFT'
+                    dangerous_bullets.append({'distance': dist_y - me['height'], 'turn': direction})
+                    self.turn_direction = direction
+            elif bullet['direction'] == 'DOWN' and (me['direction'] == 'UP' or me['direction'] == 'DOWN'):
+                if -bullet['height'] - RADIUS < dist_y < 0 and -bullet['width'] < dist_x < me['width']:
+                    direction = 'RIGHT' if dist_x + bullet['width'] // 2 < me['width'] // 2 else 'LEFT'
+                    dangerous_bullets.append({'distance': -dist_y - bullet['height'], 'turn': direction})
+                    self.turn_direction = direction
+            elif bullet['direction'] == 'LEFT' and (me['direction'] == 'LEFT' or me['direction'] == 'RIGHT'):
                 if 0 < dist_x < RADIUS + me['width'] and -bullet['height'] < dist_y < me['height']:
-                    if me['direction'] == 'LEFT' or me['direction'] == 'RIGHT':
-                        self.turn_direction = 'UP'
-            if bullet['direction'] == 'RIGHT':
-                if -RADIUS - me['width'] < dist_x < 0 and -bullet['height'] < dist_y < me['height']:
-                    if me['direction'] == 'LEFT' or me['direction'] == 'RIGHT':
-                        self.turn_direction = 'UP'
+                    direction = 'DOWN' if dist_y + bullet['height'] // 2 < me['height'] // 2 else 'UP'
+                    dangerous_bullets.append({'distance': dist_x - me['width'], 'turn': direction})
+                    self.turn_direction = direction
+            elif bullet['direction'] == 'RIGHT' and (me['direction'] == 'LEFT' or me['direction'] == 'RIGHT'):
+                if -RADIUS - bullet['width'] < dist_x < 0 and -bullet['height'] < dist_y < me['height']:
+                    direction = 'DOWN' if dist_y + bullet['height'] // 2 < me['height'] // 2 else 'UP'
+                    dangerous_bullets.append({'distance': -dist_x - bullet['width'], 'turn': direction})
+                    self.turn_direction = direction
+            elif any(me_rects[i].colliderect(bullet_rects[i]) for i in range(10)):
+                direction = opposite_direction(me['direction'])
+                dangerous_bullets.append({'distance': 0, 'turn': direction})
+                self.turn_direction = direction
+
+        for tank in tanks:
+            dist_x = tank['x'] - me['x']
+            dist_y = tank['y'] - me['y']
+            if tank['direction'] == 'UP' and me['direction'] == 'DOWN':
+                if 0 < dist_y < RADIUS + me['height'] and -tank['width'] < dist_x < me['width']:
+                    direction = 'RIGHT' if dist_x + tank['width'] // 2 < me['width'] // 2 else 'LEFT'
+                    dangerous_bullets.append({'distance': dist_y - me['height'], 'turn': direction})
+                    self.fire = True
+                    self.turn_direction = direction
+            elif tank['direction'] == 'DOWN' and me['direction'] == 'UP':
+                if -tank['height'] - RADIUS < dist_y < 0 and -tank['width'] < dist_x < me['width']:
+                    direction = 'RIGHT' if dist_x + tank['width'] // 2 < me['width'] // 2 else 'LEFT'
+                    dangerous_bullets.append({'distance': -dist_y - tank['height'], 'turn': direction})
+                    self.fire = True
+                    self.turn_direction = direction
+            elif tank['direction'] == 'LEFT' and me['direction'] == 'RIGHT':
+                if 0 < dist_x < RADIUS + me['width'] and -tank['height'] < dist_y < me['height']:
+                    direction = 'DOWN' if dist_y + tank['height'] // 2 < me['height'] // 2 else 'UP'
+                    dangerous_bullets.append({'distance': dist_x - me['width'], 'turn': direction})
+                    self.fire = True
+                    self.turn_direction = direction
+            elif tank['direction'] == 'RIGHT' and me['direction'] == 'LEFT':
+                if -RADIUS - tank['width'] < dist_x < 0 and -tank['height'] < dist_y < me['height']:
+                    direction = 'DOWN' if dist_y + tank['height'] // 2 < me['height'] // 2 else 'UP'
+                    dangerous_bullets.append({'distance': -dist_x - tank['width'], 'turn': direction})
+                    self.fire = True
+                    self.turn_direction = direction
 
 
 
@@ -88,13 +150,13 @@ def autoplay():
             bullets = cur_state['gameField']['bullets']
 
             ai.start(tanks, bullets)
+            if ai.fire:
+                rpc_response = rpc.fire()
+                ai.fire = False
             if ai.turn_direction:
                 print(ai.turn_direction)
                 rpc_response = rpc.turn_tank(ai.turn_direction)
                 ai.turn_direction = ''
-            if ai.fire:
-                rpc_response = rpc.fire()
-                ai.fire = False
 
             remaining_time = cur_state.get('remainingTime', 0)
             text = font.render(f'Remaining time: {remaining_time}', True, (0, 0, 0))
